5c62f04d3621cbef4448bb24bb5f83abbb9ab61e
njuerect <252289481@qq.com>
2018-12-14 17:33:45 +0800

    Create test_cnn_ops_foreach.sh

---
+++ layer-based_benchmark/cnn/test_cnn_ops_foreach.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+for i in `seq 0 9`;
+do
+mkdir -p log
+python cnn_ops.py -op_name conv -times 10 -interval 1
+sleep 1
+python cnn_ops.py -op_name fc -times 10 -interval 1
+sleep 1
+python cnn_ops.py -op_name max_pool -times 10 -interval 1
+sleep 1
+python cnn_ops.py -op_name avg_pool -times 10 -interval 1
+sleep 1
+python cnn_ops.py -op_name bn -times 10 -interval 1
+sleep 1
+python cnn_ops.py -op_name lrn -times 10 -interval 1
+sleep 1
+python cnn_ops.py -op_name relu -times 100 -interval 1
+sleep 1
+python cnn_ops.py -op_name sigmoid -times 100 -interval 1
+sleep 1
+python cnn_ops.py -op_name softmax -times 100 -interval 1
+sleep 1
+python cnn_ops.py -op_name tanh -times 100 -interval 1
+mv log log-$i
+sleep 5
+done
